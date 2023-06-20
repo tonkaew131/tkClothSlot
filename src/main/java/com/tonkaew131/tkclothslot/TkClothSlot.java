99890a95e6c351d2065084f2d0968c42ca6c2174
Athicha L. (mac) <nongtonkaew@gmail.com>
2023-06-20 22:31:23 +0700

    Finally Build Pass

--- src/main/java/com/tonkaew131/tkclothslot/TkClothSlot.java
+++ src/main/java/com/tonkaew131/tkclothslot/TkClothSlot.java
@@ -1,17 +1,20 @@
 package com.tonkaew131.tkclothslot;
 
+import net.kyori.adventure.text.Component;
+import org.bukkit.Bukkit;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class TkClothSlot extends JavaPlugin {
-
+public final class TkClothSlot extends JavaPlugin implements Listener {
     @Override
     public void onEnable() {
-        // Plugin startup logic
-
+        Bukkit.getPluginManager().registerEvents(this, this);
     }
 
-    @Override
-    public void onDisable() {
-        // Plugin shutdown logic
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        event.getPlayer().sendMessage(Component.text("Hello, " + event.getPlayer().getName() + "!"));
     }
 }
